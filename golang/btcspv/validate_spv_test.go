6a5626c7e5fb3a84e7dd4146b0840795785c002a
Erin Hales <erin@summa.one>
2020-02-18 17:03:48 -0700

    setup new testing groundwork, began converting tests

--- golang/btcspv/validate_spv_test.go
+++ golang/btcspv/validate_spv_test.go
@@ -1,9 +1,5 @@
 package btcspv
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-)
-
 func normalizeToByteSlice(b interface{}) []byte {
 	switch b.(type) {
 	case []byte:
@@ -19,159 +15,159 @@ func normalizeToByteSlice(b interface{}) []byte {
 	}
 }
 
-func (suite *UtilsSuite) TestProve() {
-	fixture := suite.Fixtures["prove"]
+// func (suite *UtilsSuite) TestProve() {
+// 	fixture := suite.Fixtures["prove"]
 
-	for i := range fixture {
-		testCase := fixture[i]
-		expected := testCase.Output.(bool)
-		inputs := testCase.Input.(map[string]interface{})
-		txIDLE := inputs["txIdLE"].(Hash256Digest)
-		merkleRootLE := inputs["merkleRootLE"].(Hash256Digest)
-		proof := inputs["proof"].([]byte)
-		index := uint(inputs["index"].(int))
-		actual := Prove(txIDLE, merkleRootLE, proof, index)
-		suite.Equal(expected, actual)
-	}
-}
+// 	for i := range fixture {
+// 		testCase := fixture[i]
+// 		expected := testCase.Output.(bool)
+// 		inputs := testCase.Input.(map[string]interface{})
+// 		txIDLE := inputs["txIdLE"].(Hash256Digest)
+// 		merkleRootLE := inputs["merkleRootLE"].(Hash256Digest)
+// 		proof := inputs["proof"].([]byte)
+// 		index := uint(inputs["index"].(int))
+// 		actual := Prove(txIDLE, merkleRootLE, proof, index)
+// 		suite.Equal(expected, actual)
+// 	}
+// }
 
-func (suite *UtilsSuite) TestCalculateTxId() {
-	fixture := suite.Fixtures["calculateTxId"]
+// func (suite *UtilsSuite) TestCalculateTxId() {
+// 	fixture := suite.Fixtures["calculateTxId"]
 
-	for i := range fixture {
-		testCase := fixture[i]
-		expected := testCase.Output.(Hash256Digest)
-		inputs := testCase.Input.(map[string]interface{})
-		version := inputs["version"].([]byte)
-		vin := inputs["vin"].([]byte)
-		vout := inputs["vout"].([]byte)
-		locktime := inputs["locktime"].([]byte)
-		actual := CalculateTxID(version, vin, vout, locktime)
-		suite.Equal(expected, actual)
-	}
-}
+// 	for i := range fixture {
+// 		testCase := fixture[i]
+// 		expected := testCase.Output.(Hash256Digest)
+// 		inputs := testCase.Input.(map[string]interface{})
+// 		version := inputs["version"].([]byte)
+// 		vin := inputs["vin"].([]byte)
+// 		vout := inputs["vout"].([]byte)
+// 		locktime := inputs["locktime"].([]byte)
+// 		actual := CalculateTxID(version, vin, vout, locktime)
+// 		suite.Equal(expected, actual)
+// 	}
+// }
 
-func (suite *UtilsSuite) TestParseInput() {
-	fixture := suite.Fixtures["parseInput"]
+// func (suite *UtilsSuite) TestParseInput() {
+// 	fixture := suite.Fixtures["parseInput"]
 
-	for i := range fixture {
-		testCase := fixture[i]
-		expected := testCase.Output.(map[string]interface{})
-		expectedSequence := uint(expected["sequence"].(int))
-		expectedTxID := expected["txId"].(Hash256Digest)
-		expectedIndex := uint(expected["index"].(int))
-		expectedType := InputType(expected["type"].(int))
-		input := testCase.Input.([]byte)
-		actualSequence, actualTxID, actualIndex, actualType := ParseInput(input)
-		suite.Equal(expectedSequence, actualSequence)
-		suite.Equal(expectedTxID, actualTxID)
-		suite.Equal(expectedIndex, actualIndex)
-		suite.Equal(expectedType, actualType)
-	}
-}
+// 	for i := range fixture {
+// 		testCase := fixture[i]
+// 		expected := testCase.Output.(map[string]interface{})
+// 		expectedSequence := uint(expected["sequence"].(int))
+// 		expectedTxID := expected["txId"].(Hash256Digest)
+// 		expectedIndex := uint(expected["index"].(int))
+// 		expectedType := InputType(expected["type"].(int))
+// 		input := testCase.Input.([]byte)
+// 		actualSequence, actualTxID, actualIndex, actualType := ParseInput(input)
+// 		suite.Equal(expectedSequence, actualSequence)
+// 		suite.Equal(expectedTxID, actualTxID)
+// 		suite.Equal(expectedIndex, actualIndex)
+// 		suite.Equal(expectedType, actualType)
+// 	}
+// }
 
-func (suite *UtilsSuite) TestParseOutput() {
-	fixture := suite.Fixtures["parseOutput"]
+// func (suite *UtilsSuite) TestParseOutput() {
+// 	fixture := suite.Fixtures["parseOutput"]
 
-	for i := range fixture {
-		testCase := fixture[i]
-		expected := testCase.Output.(map[string]interface{})
-		expectedValue := uint(expected["value"].(int))
-		expectedOutputType := OutputType(expected["type"].(int))
-		expectedPayload := normalizeToByteSlice(expected["payload"])
-		input := normalizeToByteSlice(testCase.Input)
-		actualValue, actualOutputType, actualPayload := ParseOutput(input)
-		suite.Equal(expectedValue, actualValue)
-		suite.Equal(expectedPayload, actualPayload)
-		suite.Equal(expectedOutputType, actualOutputType)
-	}
-}
+// 	for i := range fixture {
+// 		testCase := fixture[i]
+// 		expected := testCase.Output.(map[string]interface{})
+// 		expectedValue := uint(expected["value"].(int))
+// 		expectedOutputType := OutputType(expected["type"].(int))
+// 		expectedPayload := normalizeToByteSlice(expected["payload"])
+// 		input := normalizeToByteSlice(testCase.Input)
+// 		actualValue, actualOutputType, actualPayload := ParseOutput(input)
+// 		suite.Equal(expectedValue, actualValue)
+// 		suite.Equal(expectedPayload, actualPayload)
+// 		suite.Equal(expectedOutputType, actualOutputType)
+// 	}
+// }
 
-func (suite *UtilsSuite) TestParseHeader() {
-	fixture := suite.Fixtures["parseHeader"]
+// func (suite *UtilsSuite) TestParseHeader() {
+// 	fixture := suite.Fixtures["parseHeader"]
 
-	for i := range fixture {
-		testCase := fixture[i]
-		expected := testCase.Output.(map[string]interface{})
-		expectedDigest := expected["digest"].(Hash256Digest)
-		expectedVersion := uint(expected["version"].(int))
-		expectedPrevHash := expected["prevHash"].(Hash256Digest)
-		expectedMerkleRoot := expected["merkleRoot"].(Hash256Digest)
-		expectedTimestamp := uint(expected["timestamp"].(int))
-		expectedTarget := BytesToBigUint(expected["target"].([]byte))
-		expectedNonce := uint(expected["nonce"].(int))
-		input := testCase.Input.(RawHeader)
-		actualDigest, actualVersion, actualPrevHash, actualMerkleRoot, actualTimestamp, actualTarget, actualNonce, err := ParseHeader(input)
-		suite.Nil(err)
-		suite.Equal(expectedDigest, actualDigest)
-		suite.Equal(expectedVersion, actualVersion)
-		suite.Equal(expectedPrevHash, actualPrevHash)
-		suite.Equal(expectedMerkleRoot, actualMerkleRoot)
-		suite.Equal(expectedTimestamp, actualTimestamp)
-		suite.Equal(expectedTarget, actualTarget)
-		suite.Equal(expectedNonce, actualNonce)
-	}
-}
+// 	for i := range fixture {
+// 		testCase := fixture[i]
+// 		expected := testCase.Output.(map[string]interface{})
+// 		expectedDigest := expected["digest"].(Hash256Digest)
+// 		expectedVersion := uint(expected["version"].(int))
+// 		expectedPrevHash := expected["prevHash"].(Hash256Digest)
+// 		expectedMerkleRoot := expected["merkleRoot"].(Hash256Digest)
+// 		expectedTimestamp := uint(expected["timestamp"].(int))
+// 		expectedTarget := BytesToBigUint(expected["target"].([]byte))
+// 		expectedNonce := uint(expected["nonce"].(int))
+// 		input := testCase.Input.(RawHeader)
+// 		actualDigest, actualVersion, actualPrevHash, actualMerkleRoot, actualTimestamp, actualTarget, actualNonce, err := ParseHeader(input)
+// 		suite.Nil(err)
+// 		suite.Equal(expectedDigest, actualDigest)
+// 		suite.Equal(expectedVersion, actualVersion)
+// 		suite.Equal(expectedPrevHash, actualPrevHash)
+// 		suite.Equal(expectedMerkleRoot, actualMerkleRoot)
+// 		suite.Equal(expectedTimestamp, actualTimestamp)
+// 		suite.Equal(expectedTarget, actualTarget)
+// 		suite.Equal(expectedNonce, actualNonce)
+// 	}
+// }
 
-func (suite *UtilsSuite) TestValidateHeaderWork() {
-	var target sdk.Uint
-	fixture := suite.Fixtures["validateHeaderWork"]
+// func (suite *UtilsSuite) TestValidateHeaderWork() {
+// 	var target sdk.Uint
+// 	fixture := suite.Fixtures["validateHeaderWork"]
 
-	for i := range fixture {
-		testCase := fixture[i]
-		expected := testCase.Output.(bool)
-		inputs := testCase.Input.(map[string]interface{})
-		digest := inputs["digest"].(Hash256Digest)
+// 	for i := range fixture {
+// 		testCase := fixture[i]
+// 		expected := testCase.Output.(bool)
+// 		inputs := testCase.Input.(map[string]interface{})
+// 		digest := inputs["digest"].(Hash256Digest)
 
-		switch inputs["target"].(type) {
-		case int:
-			targetInt := inputs["target"].(int)
-			target = sdk.NewUint(uint64(targetInt))
-		case Hash256Digest:
-			buf := inputs["target"].(Hash256Digest)
-			target = BytesToBigUint(buf[:])
-		case []byte:
-			buf := inputs["target"].([]byte)
-			target = BytesToBigUint(buf[:])
-		}
+// 		switch inputs["target"].(type) {
+// 		case int:
+// 			targetInt := inputs["target"].(int)
+// 			target = sdk.NewUint(uint64(targetInt))
+// 		case Hash256Digest:
+// 			buf := inputs["target"].(Hash256Digest)
+// 			target = BytesToBigUint(buf[:])
+// 		case []byte:
+// 			buf := inputs["target"].([]byte)
+// 			target = BytesToBigUint(buf[:])
+// 		}
 
-		actual := ValidateHeaderWork(digest, target)
-		suite.Equal(expected, actual)
-	}
-}
+// 		actual := ValidateHeaderWork(digest, target)
+// 		suite.Equal(expected, actual)
+// 	}
+// }
 
-func (suite *UtilsSuite) TestValidateHeaderPrevHash() {
-	fixture := suite.Fixtures["validateHeaderPrevHash"]
+// func (suite *UtilsSuite) TestValidateHeaderPrevHash() {
+// 	fixture := suite.Fixtures["validateHeaderPrevHash"]
 
-	for i := range fixture {
-		testCase := fixture[i]
-		expected := testCase.Output.(bool)
-		inputs := testCase.Input.(map[string]interface{})
-		header := inputs["header"].(RawHeader)
-		prevHash := inputs["prevHash"].(Hash256Digest)
-		actual := ValidateHeaderPrevHash(header, prevHash)
-		suite.Equal(expected, actual)
-	}
-}
+// 	for i := range fixture {
+// 		testCase := fixture[i]
+// 		expected := testCase.Output.(bool)
+// 		inputs := testCase.Input.(map[string]interface{})
+// 		header := inputs["header"].(RawHeader)
+// 		prevHash := inputs["prevHash"].(Hash256Digest)
+// 		actual := ValidateHeaderPrevHash(header, prevHash)
+// 		suite.Equal(expected, actual)
+// 	}
+// }
 
-func (suite *UtilsSuite) TestValidateHeaderChain() {
-	fixture := suite.Fixtures["validateHeaderChain"]
+// func (suite *UtilsSuite) TestValidateHeaderChain() {
+// 	fixture := suite.Fixtures["validateHeaderChain"]
 
-	for i := range fixture {
-		testCase := fixture[i]
-		expected := sdk.NewUint(uint64(testCase.Output.(int)))
-		actual, err := ValidateHeaderChain(testCase.Input.([]byte))
-		suite.Nil(err)
-		suite.Equal(expected, actual)
-	}
+// 	for i := range fixture {
+// 		testCase := fixture[i]
+// 		expected := sdk.NewUint(uint64(testCase.Output.(int)))
+// 		actual, err := ValidateHeaderChain(testCase.Input.([]byte))
+// 		suite.Nil(err)
+// 		suite.Equal(expected, actual)
+// 	}
 
-	fixture = suite.Fixtures["validateHeaderChainError"]
+// 	fixture = suite.Fixtures["validateHeaderChainError"]
 
-	for i := range fixture {
-		testCase := fixture[i]
-		expected := testCase.ErrorMessage.(string)
-		actual, err := ValidateHeaderChain(testCase.Input.([]byte))
-		suite.EqualError(err, expected)
-		suite.Equal(actual, sdk.NewUint(0))
-	}
-}
+// 	for i := range fixture {
+// 		testCase := fixture[i]
+// 		expected := testCase.ErrorMessage.(string)
+// 		actual, err := ValidateHeaderChain(testCase.Input.([]byte))
+// 		suite.EqualError(err, expected)
+// 		suite.Equal(actual, sdk.NewUint(0))
+// 	}
+// }
